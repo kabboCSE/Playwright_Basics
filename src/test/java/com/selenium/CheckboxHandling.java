f8ce25da04f42287c569e8df0e7866f6f3d51b36
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-03-17 01:49:43 +0600

    Checkbox element

---
+++ src/test/java/com/selenium/CheckboxHandling.java
@@ -0,0 +1,67 @@
+package com.selenium;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CheckboxHandling {
+    Playwright playwright;
+    BrowserType browserType;
+    Browser browser;
+    BrowserContext browserContext;
+    Page page;
+
+    @BeforeSuite
+    public void start(){
+        playwright = Playwright.create();
+        browserType = playwright.chromium();
+        browser = browserType.launch(new BrowserType.LaunchOptions()
+                .setHeadless(false)
+                .setArgs(Arrays.asList(new String[]{"--start-maximized"})));
+        browserContext = browser.newContext(new Browser.NewContextOptions()
+                .setViewportSize(null));
+        page = browser.newPage();
+        System.out.println("Browser version: " + browser.version());
+    }
+
+    @Test
+    public void openUrl() throws InterruptedException {
+        Thread.sleep(2000);
+        page.navigate("https://testing-and-learning-hub.vercel.app/Selenium/pages/registration_form.html");
+        Thread.sleep(5000);
+    }
+
+    //@Test(dependsOnMethods = "openUrl")
+    public void clickOnCheckbox() throws InterruptedException {
+        ElementHandle element = page.querySelector("//input[@id='hobby1']");
+        element.click();
+        Thread.sleep(2000);
+    }
+
+    @Test(dependsOnMethods = "openUrl")
+    public void clickMultipleCheckbox() throws InterruptedException {
+        List<ElementHandle> element = page.querySelectorAll("//input[@type=\'checkbox\']");
+        for (ElementHandle checkbox : element) {
+            if (!checkbox.isChecked()) {
+                checkbox.click();
+                Thread.sleep(1000);
+            }
+        }
+
+        Thread.sleep(2000);
+    }
+
+
+
+    @AfterSuite
+    public void stop(){
+        page.close();
+        browser.close();
+        playwright.close();
+
+    }
+}
